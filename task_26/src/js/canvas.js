a7857355a7fec723b5da48c9cbfe832997d2e619
LeeYushan <hdflixue@sina.cn>
2021-02-18 22:04:18 +0800

    task 26-canvas_element

--- task_26/src/js/canvas.js
+++ task_26/src/js/canvas.js
@@ -47,7 +47,7 @@ export class World extends EventEmitter {
         map(this.elements, (it) => {
             this.ctx.save();
             it.render();
-            this.ctx.reload();
+            this.ctx.restore();
         })
     }
 
@@ -59,6 +59,104 @@ export class World extends EventEmitter {
         this.width = this.canvas.width = this.viewport.clientWidth;
         this.height = this.canvas.height = this.viewport.clientHeight;
         this.emit('resize')
+    }
+}
+
+export class Element extends EventEmitter {
+    static ID = 0;
+    x = 0;
+    y = 0;
+    r = 0;
+    width = 1;
+    height = 1;
+    animateTimer = [];
+    children = []
+
+    constructor() {
+        super();
+        this.id = ++Element.ID;
+    }
+
+    draw() {
+
+    }
+
+    renderChildren() {
+        this.children.forEach((it) => {
+            this.ctx.save();
+            it.render();
+            this.ctx.restore();
+        })
+    }
+
+    render() {
+        this.ctx.translate(this.x, this.y);
+        this.ctx.rotate(this.r);
+        this.ctx.save();
+        this.draw();
+        this.ctx.restore();
+        this.renderChildren();
+    }
+
+    clearAnimate() {
+        while (this.animateTimer.length) {
+            let timer = this.animateTimer.shift();
+            clearTimeout(timer);
+        }
+    }
+
+    moveTo(x, y, r = this.r, time = 0) {
+        if (time) {
+            this.emit('move', {x: x, y: y, r: r, time: time});
+            this.clearAnimate();
+
+            let count = Math.ceil(time / 30);
+            let perTime = time / count;
+            let diffX = x / count;
+            let diffY = y / count;
+            let diffR = r / count;
+
+            for (let i = 1; i <= count; i++) {
+                let _x = this.x + diffX * i;
+                let _y = this.y + diffY * i;
+                let _r = this.r + diffR * i;
+
+                this.animateTimer.push(
+                    setTimeout(() => {
+                        this.x = _x;
+                        this.y = _y;
+                        this.r = _r;
+                        if (i === count) {
+                            this.emit('moved', {x: x, y: y, r: r, time: time})
+                        }
+                    }, perTime * i)
+                )
+            }
+        } else {
+            this.x = x;
+            this.y = y;
+            this.r = r;
+        }
+    }
+
+    remove() {
+        this.world.remove(this);
+    }
+
+    prepend(element) {
+        this.children.unshift(element);
+    }
+
+    append(element) {
+        this.children.push(element);
+    }
+
+    prependTo(target) {
+        target.prepend(this);
+    }
+
+    appendTo(target) {
+        target.append(this);
     }
 }
 
